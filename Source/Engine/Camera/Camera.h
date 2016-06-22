89f8bec8decb5dd5969e7f47fd1528ab21442866
Luc Charbonneau <charbonneau.luc@outlook.com>
2016-06-21 23:58:57 -0400

    test

---
+++ Source/Engine/Camera/Camera.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glm\glm.hpp>
+
+namespace Engine
+{
+	class Camera
+	{
+	public:
+		Camera(const glm::vec3& position, const glm::vec3& POI, const glm::vec3& up);
+		virtual ~Camera(){};
+
+		//Getters
+		void GetViewMatrix(glm::mat4x4& matrix);
+		glm::mat4x4 GetViewMatrix();
+		glm::vec3 GetPosition() { return m_position; }
+		glm::vec3 GetPOI() { return m_POI; }
+		glm::vec3 GetLookAt() { return glm::normalize(m_POI - m_position); }
+		glm::vec3 GetUp() { return m_up; }
+
+		void SetViewMatrix(const glm::vec3& position, const glm::vec3& POI, const glm::vec3& up);
+		void SetPosition(const glm::vec3& position) { m_position = position; }
+
+	protected:
+		glm::mat4x4 m_viewMatrix;
+
+		glm::vec3 m_position;
+		glm::vec3 m_POI;
+		glm::vec3 m_up;
+	};
+}
